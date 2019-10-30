30a0e7b51512ea7652f466cb4e7561572a29934c
William Bellavnce <flintman@flintmancomputers.com>
2019-10-30 09:52:27 -0400

    removed eng tag

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -93,7 +93,7 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_STEM  := ueventd.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
